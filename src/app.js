5544cf5baf3670beb04d50c02793d3871a789f3f
chrisstophe <christophe.rezk@gmail.com>
2022-06-17 10:13:28 -0400

    Refactored reading/writing from/to the accounts and users files to data.js

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@ const path = require("path");
 const express = require("express");
 const { __ } = require("ramda");
 const { request } = require("http");
+const { accounts, users, writeJSON } = require("./data.js");
 
 const app = express();
 
@@ -11,12 +12,6 @@ app.set("view engine", "ejs");
 
 app.use(express.static(`${__dirname}/public`));
 app.use(express.urlencoded({ extended: true }));
-
-const accountData = fs.readFileSync(`${__dirname}/json/accounts.json`, "utf8");
-const accounts = JSON.parse(accountData);
-
-const userData = fs.readFileSync(`${__dirname}/json/users.json`, "utf8");
-const users = JSON.parse(userData);
 
 app.get("/", (req, res) => {
   res.render("index", { title: "Account Summary", accounts: accounts });
@@ -42,12 +37,7 @@ app.post("/transfer", (req, res) => {
   const amount = +req.body.amount;
   accounts[from].balance -= amount;
   accounts[to].balance += parseInt(amount);
-  const accountsJSON = JSON.stringify(accounts);
-  fs.writeFileSync(
-    path.join(__dirname, "json/accounts.json"),
-    accountsJSON,
-    "utf8"
-  );
+  writeJSON();
   res.render("transfer", { message: "Transfer Completed" });
 });
 app.get("/payment", (req, res) => {
@@ -56,12 +46,7 @@ app.get("/payment", (req, res) => {
 app.post("/payment", (req, res) => {
   accounts.credit.balance -= parseInt(req.body.amount);
   accounts.credit.available += parseInt(req.body.amount);
-  const accountsJSON = JSON.stringify(accounts);
-  fs.writeFileSync(
-    path.join(__dirname, "json/accounts.json"),
-    accountsJSON,
-    "utf8"
-  );
+  writeJSON();
   res.render("payment", {
     message: "Payment Successful",
     account: accounts.credit,
